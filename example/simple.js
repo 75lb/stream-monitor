88f5a8041b0b4f0fdd8fbc648039d87174d3da36
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-03-21 20:48:26 +0000

    readme

--- example/simple.js
+++ example/simple.js
@@ -1,7 +1,7 @@
 "use strict";
-var monitor = require("../"),
-    fs = require("fs");
+var monitor = require("../");
+var fs = require("fs");
 
-var file  = fs. createReadStream(__filename);
+var file = fs.createReadStream(__filename);
 monitor(file);
 file.pipe(fs.createWriteStream("dupe.js"));
